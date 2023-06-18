ff129167336e397e047105de48bfd619f4ebcd8f
anoderb <khamdanusyakir169h@gmail.com>
2023-06-18 11:08:18 +0700

    Leading Update

--- constants/index.js
+++ constants/index.js
@@ -26,7 +26,7 @@ export const navLinks = [
   },
   {
     id: "product",
-    title: "Product",
+    title: "More",
   },
   {
     id: "clients",
@@ -38,7 +38,7 @@ export const features = [
   {
     id: "feature-1",
     icon: star,
-    title: "Rewards",
+    title: "Better Pricing",
     content:
       "The best credit cards offer some tantalizing combinations of promotions and prizes",
   },
@@ -52,7 +52,7 @@ export const features = [
   {
     id: "feature-3",
     icon: send,
-    title: "Balance Transfer",
+    title: "Fast Transtaction",
     content:
       "A balance transfer credit card can save you a lot of money in interest charges.",
   },
@@ -62,26 +62,26 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
-    title: "Founder & Leader",
-    img: people01,
+      "Decentralized Exchage",
+    name: "Swap token and collectible.",
+    title: "Cooming soon",
+    
   },
   {
     id: "feedback-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
-    title: "Founder & Leader",
-    img: people02,
+      "Liquidity pools",
+    name: "Provide liquidity and Earn rewards.",
+    title: "Cooming soon",
+    
   },
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
-    title: "Founder & Leader",
-    img: people03,
+      "Wrapped",
+    name: "Bitcoin, Etherium.",
+    title: "Cooming soon",
+   
   },
 ];
 
