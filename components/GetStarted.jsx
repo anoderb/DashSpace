0e9490cfc3b6585f465c7e199ab6aa96cadb4704
anoderb <khamdanusyakir169h@gmail.com>
2023-06-25 10:53:40 +0700

    update homepage test

--- components/GetStarted.jsx
+++ components/GetStarted.jsx
@@ -3,7 +3,7 @@ import { arrowUp } from "@/public/assets";
 import Image from "next/image";
 
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+  <div className={`${styles.flexCenter} w-[100px] h-[100px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,36 +14,36 @@ const hideOnMdStyles = css`
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-        <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraph} ml-2`}>
-          <span className="text-white">20%</span> Discount For{' '}
-          <span className="text-white">1 Month</span> Account
+    <section id="home" className={`flex md:flex-row flex-col md:items-center ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-16 sm:px-16 px-6 md:w-1/2`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> Discount For{' '}
+            <span className="text-white">1 Month</span> Account
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            Dashboard To <br className="sm:block hidden" />{' '}
+            <span className="text-gradient">Interchain</span>{' '}
+          </h1>
+          {/* <div className="md:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div> */}
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Integrations.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Swap ,Bridge, Earn and Build on leading decentralize crypto trading protocol with interchain integration
         </p>
       </div>
-
-      <div className="flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          Dashboard To <br className="sm:block hidden" />{' '}
-          <span className="text-gradient">Interchain</span>{' '}
-        </h1>
-        <div className="md:flex hidden md:mr-4 mr-0">
-          <GetStarted />
+        <div className="flex md:w-1/2 md:pl-8" style={{ height: '100%' }}>
+          <Image src={robot} alt="billing" className="w-full h-full" />
         </div>
-      </div>
-
-      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-        Integrations.
-      </h1>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-      Swap ,Bridge, Earn and Build on leading decentralize crypto trading protocol with interchain integration
-      </p>
-    </div>
-    <div className="flex" style={{ height: '100%' }}>
-      <Image src={robot} alt="billing" className="w-full h-full" />
-    </div>
 
       <div className={`flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         {/* <Image src={robot} alt="billing" className="w-[100%] object-cover h-[100%] relative z-[5] " /> */}
@@ -55,9 +55,9 @@ const Hero = () => {
         {/* gradient end */}
       </div>
       <div className={`md:hidden flex ${styles.flexCenter}`}>
-        <div className="sm:hidden">
+        {/* <div className="sm:hidden">
           <GetStarted />
-        </div>
+        </div> */}
       </div>
     </section>
   );
